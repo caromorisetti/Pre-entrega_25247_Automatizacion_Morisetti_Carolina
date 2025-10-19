b7637b99c6dfb745b6e10374b9327101be27dbc1
caromorisetti <164552832+caromorisetti@users.noreply.github.com>
2025-10-19 12:09:46 +0200

    Cambios test_login

--- test_login.py
+++ test_login.py
@@ -2,20 +2,23 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(5)
+def test_login():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
 
-try:
-    # Login
-     driver.get("https://www.saucedemo.com/")
-     driver.find_element(By.ID, "user-name").send_keys("standard_user")
-     driver.find_element(By.ID, "password").send_keys("secret_sauce")
-     driver.find_element(By.ID, "login-button").click()
-     
-     # Validacion de la redireccion de la pagina
-     assert '/inventory.html' in driver.current_url
-     
-     print("Login exitoso")
-     
-finally:
-    driver.quit()
+    try:
+        # Login
+        driver.get("https://www.saucedemo.com/")
+        time.sleep(3)
+        driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        driver.find_element(By.ID, "login-button").click()
+        time.sleep(3)
+        # Validacion de la redireccion de la pagina
+        assert '/inventory.html' in driver.current_url    
+        print("Login exitoso y validado correctamente")
+    except Exception as e:
+        print("Error durante el login:", e)
+        raise
+    finally:
+        driver.quit()
